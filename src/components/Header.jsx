865db20384e61ff59ca61d7533fdb7cc40be9d71
Maya N <nettathm@gmail.com>
2023-10-25 22:55:20 -0600

    nav bar toggler shows up only on smaller screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import NavTabs from "./Navigation";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JavaScript
+
 
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <a className="navbar-brand text-white">Maya Nettath</a>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
           <NavTabs />
         </div>
       </div>
