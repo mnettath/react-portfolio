bcf778f93a2388ed8c1e1af60fdac2f3fa9f8d1a
Maya N <nettathm@gmail.com>
2023-10-25 20:27:45 -0600

    navigation component renders sections without refreshing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,17 @@
 import React from "react";
+import NavTabs from "./Navigation";
 
 function Header() {
-  return (<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <a class="navbar-brand text-white">Maya Nettath</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active text-white" aria-current="page" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="#">Portfolio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="#">Resume</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-)
+  return (
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white">Maya Nettath</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <NavTabs />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
+
+function NavTabs() {
+  const currentPage = useLocation().pathname;
+
+  return (
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <Link
+          to="/"
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === '/' ? 'nav-link active text-white' : 'nav-link text-white'}
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/portfolio"
+          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === '/Blog' ? 'nav-link active text-white' : 'nav-link text-white'}
+        >
+          Portfolio
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/resume"
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === '/Contact' ? 'nav-link active text-white' : 'nav-link text-white'}
+        >
+          Resume
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/contact"
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === '/Contact' ? 'nav-link active text-white' : 'nav-link text-white'}
+        >
+          Contact
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
