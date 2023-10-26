8d540e13fe3031d4a76741508e3167971eb487a5
Maya N <nettathm@gmail.com>
2023-10-25 19:13:54 -0600

    imported react to each page/component, also added headr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,31 @@
-const styles = {
-  backgroundColor: "blue",
-};
+import React from "react";
 
 function Header() {
-  return (
-    <div style={styles.headerContainer}>
-      <p>This is the header!</p>
+  return (<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand">Maya Nettath</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
     </div>
-  );
+  </div>
+</nav>
+)
 }
 
 export default Header;
