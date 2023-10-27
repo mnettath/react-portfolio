18abd20f3d6ad3eccaf1a872ad2f03e5fcaf74c0
Maya N <nettathm@gmail.com>
2023-10-27 15:26:06 -0600

    started to set up cards on portfolio page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className="mt-3 ms-3">
+    <div className="mt-3 mx-3">
       <h3 className="mb-3">About Me</h3>
       <img
         src="src\assets\Maya_Profile.jpg"
-        className="img-thumbnail img-fluid w-25"
+        className="img-thumbnail img-fluid w-25 mb-3"
         alt="profile picture of Maya Nettath"
       ></img>
-      <div className = "about-text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis libero congue nisi consequat cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris arcu odio, ultrices bibendum tortor eu, posuere lobortis nisl. Pellentesque auctor, magna at mattis posuere, orci nisl rhoncus elit, ac tincidunt enim felis id felis. Cras ut tellus tellus. Nullam purus enim, convallis eget accumsan ut, egestas in metus. Duis pulvinar suscipit ultrices. Curabitur egestas dictum aliquam. In hac habitasse platea dictumst. Quisque sollicitudin pharetra leo quis gravida. Fusce iaculis lorem eget semper placerat. Donec fermentum risus cursus sapien porttitor vehicula. Aliquam eget ligula et est sollicitudin eleifend in id neque. Vestibulum a orci ac dui mattis convallis quis non lectus. Sed eleifend at mi a volutpat. Proin aliquet iaculis erat, a interdum justo ultricies ac.</p></div>
+      <div className="about-text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          facilisis libero congue nisi consequat cursus. Vestibulum ante ipsum
+          primis in faucibus orci luctus et ultrices posuere cubilia curae;
+          Mauris arcu odio, ultrices bibendum tortor eu, posuere lobortis nisl.
+          Pellentesque auctor, magna at mattis posuere, orci nisl rhoncus elit,
+          ac tincidunt enim felis id felis. Cras ut tellus tellus. Nullam purus
+          enim, convallis eget accumsan ut, egestas in metus. Duis pulvinar
+          suscipit ultrices. Curabitur egestas dictum aliquam. In hac habitasse
+          platea dictumst. Quisque sollicitudin pharetra leo quis gravida. Fusce
+          iaculis lorem eget semper placerat. Donec fermentum risus cursus
+          sapien porttitor vehicula. Aliquam eget ligula et est sollicitudin
+          eleifend in id neque. Vestibulum a orci ac dui mattis convallis quis
+          non lectus. Sed eleifend at mi a volutpat. Proin aliquet iaculis erat,
+          a interdum justo ultricies ac.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,11 +1,127 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Portfolio = () => {
   return (
-    <div>
-      Portfolio
+    <div className="container mt-3 mx-3">
+      <h3 className="mb-3">Portfolio</h3>
+      <div className="row">
+        <div className="col">
+          <div className="card" style={{ width: "14rem", height: "23rem" }}>
+            <img
+              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Deployed Page
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card" style={{ width: "14rem", height: "23rem" }}>
+            <img
+              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Deployed Page
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card" style={{ width: "14rem", height: "23rem" }}>
+            <img
+              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Deployed Page
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card" style={{ width: "14rem", height: "23rem" }}>
+            <img
+              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Deployed Page
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card" style={{ width: "14rem", height: "23rem" }}>
+            <img
+              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Deployed Page
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card" style={{ width: "14rem", height: "23rem" }}>
+            <img
+              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Deployed Page
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
