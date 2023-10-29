4f10360d838710bf896396a22d4d2726587fd0ae
Maya N <nettathm@gmail.com>
2023-10-29 14:43:20 -0600

    imported eventful sky image to portfolio.js, altered path

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
+import EventfulSky from "../assets/images/EventfulSky.png";
 
 const Portfolio = () => {
   return (
@@ -10,17 +11,23 @@ const Portfolio = () => {
         <div className="col-sm-6 col-md-4 col-lg-3">
           <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
-              src="src\assets\images\EventfulSky.png"
+              src={EventfulSky}
               className="card-img-top img-thumbnail"
               alt="eventful sky app concert page"
             />
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Eventful Sky Weather Planner</h5>
               <div className="mt-auto">
-                <a href="https://nlad218.github.io/Eventful-sky/index.html" className="btn btn-primary mb-2">
+                <a
+                  href="https://nlad218.github.io/Eventful-sky/index.html"
+                  className="btn btn-primary mb-2"
+                >
                   Deployed Page
                 </a>
-                <a href="https://github.com/nlad218/Eventful-sky" className="btn btn-secondary">
+                <a
+                  href="https://github.com/nlad218/Eventful-sky"
+                  className="btn btn-secondary"
+                >
                   GitHub Repo
                 </a>
               </div>
@@ -39,10 +46,16 @@ const Portfolio = () => {
                 Little Site of Horrors Movie Database
               </h5>
               <div className="mt-auto">
-                <a href="https://little-site-of-horrors-1-f45120ac21d1.herokuapp.com/" className="btn btn-primary mb-2">
+                <a
+                  href="https://little-site-of-horrors-1-f45120ac21d1.herokuapp.com/"
+                  className="btn btn-primary mb-2"
+                >
                   Deployed Page
                 </a>
-                <a href="https://github.com/ljkahn/littleSiteOfHorrors" className="btn btn-secondary">
+                <a
+                  href="https://github.com/ljkahn/littleSiteOfHorrors"
+                  className="btn btn-secondary"
+                >
                   GitHub Repo
                 </a>
               </div>
@@ -59,10 +72,16 @@ const Portfolio = () => {
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">JavaScript Quiz</h5>
               <div className="mt-auto">
-                <a href="https://mnettath.github.io/code-quiz/" className="btn btn-primary mb-2">
+                <a
+                  href="https://mnettath.github.io/code-quiz/"
+                  className="btn btn-primary mb-2"
+                >
                   Deployed Page
                 </a>
-                <a href="https://github.com/mnettath/code-quiz" className="btn btn-secondary">
+                <a
+                  href="https://github.com/mnettath/code-quiz"
+                  className="btn btn-secondary"
+                >
                   GitHub Repo
                 </a>
               </div>
@@ -79,10 +98,16 @@ const Portfolio = () => {
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Employee Calendar</h5>
               <div className="mt-auto">
-                <a href="https://mnettath.github.io/employee-calendar/" className="btn btn-primary mb-2">
+                <a
+                  href="https://mnettath.github.io/employee-calendar/"
+                  className="btn btn-primary mb-2"
+                >
                   Deployed Page
                 </a>
-                <a href="https://github.com/mnettath/employee-calendar" className="btn btn-secondary">
+                <a
+                  href="https://github.com/mnettath/employee-calendar"
+                  className="btn btn-secondary"
+                >
                   GitHub Repo
                 </a>
               </div>
@@ -99,10 +124,16 @@ const Portfolio = () => {
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Weather Forecast</h5>
               <div className="mt-auto">
-                <a href="https://mnettath.github.io/weather-forecast/" className="btn btn-primary mb-2">
+                <a
+                  href="https://mnettath.github.io/weather-forecast/"
+                  className="btn btn-primary mb-2"
+                >
                   Deployed Page
                 </a>
-                <a href="https://github.com/mnettath/weather-forecast" className="btn btn-secondary">
+                <a
+                  href="https://github.com/mnettath/weather-forecast"
+                  className="btn btn-secondary"
+                >
                   GitHub Repo
                 </a>
               </div>
@@ -119,10 +150,16 @@ const Portfolio = () => {
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Note Taker</h5>
               <div className="mt-auto">
-                <a href="https://small-business-note-app-bbd345fcd895.herokuapp.com/notes" className="btn btn-primary mb-2">
+                <a
+                  href="https://small-business-note-app-bbd345fcd895.herokuapp.com/notes"
+                  className="btn btn-primary mb-2"
+                >
                   Deployed Page
                 </a>
-                <a href="https://github.com/mnettath/note-taker-app" className="btn btn-secondary">
+                <a
+                  href="https://github.com/mnettath/note-taker-app"
+                  className="btn btn-secondary"
+                >
                   GitHub Repo
                 </a>
               </div>
