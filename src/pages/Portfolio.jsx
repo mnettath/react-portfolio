58e5e397f02c0e4fd93ece8958b536d13b4e3502
Maya N <nettathm@gmail.com>
2023-10-29 12:54:58 -0600

    added portfolio images and links, formatted cards

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,20 +7,20 @@ const Portfolio = () => {
     <div className="container mt-3 mb-3 mx-3">
       <h3 className="mb-3">Portfolio</h3>
       <div className="row gy-5">
-        <div className="col-sm-6 col-s- col-md-4 col-lg-3">
-          <div className="card" style={{ width: "16rem", height: "21rem" }}>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
-              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top"
+              src="src\assets\images\EventfulSky.png"
+              className="card-img-top img-thumbnail"
               alt="..."
             />
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Eventful Sky Weather Planner</h5>
               <div className="mt-auto">
-                <a href="#" className="btn btn-primary mb-2">
+                <a href="https://nlad218.github.io/Eventful-sky/index.html" className="btn btn-primary mb-2">
                   Deployed Page
                 </a>
-                <a href="#" className="btn btn-secondary">
+                <a href="https://github.com/nlad218/Eventful-sky" className="btn btn-secondary">
                   GitHub Repo
                 </a>
               </div>
@@ -28,10 +28,10 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="card" style={{ width: "16rem", height: "21rem" }}>
+          <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
-              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top"
+              src="src\assets\images\LittleSiteOfHorrors.png"
+              className="card-img-top img-thumbnail"
               alt="..."
             />
             <div className="card-body d-flex flex-column">
@@ -39,10 +39,10 @@ const Portfolio = () => {
                 Little Site of Horrors Movie Database
               </h5>
               <div className="mt-auto">
-                <a href="#" className="btn btn-primary mb-2">
+                <a href="https://little-site-of-horrors-1-f45120ac21d1.herokuapp.com/" className="btn btn-primary mb-2">
                   Deployed Page
                 </a>
-                <a href="#" className="btn btn-secondary">
+                <a href="https://github.com/ljkahn/littleSiteOfHorrors" className="btn btn-secondary">
                   GitHub Repo
                 </a>
               </div>
@@ -50,19 +50,19 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="card" style={{ width: "16rem", height: "21rem" }}>
+          <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
-              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top"
+              src="src\assets\images\CodeQuiz.png"
+              className="card-img-top img-thumbnail"
               alt="..."
             />
             <div className="card-body d-flex flex-column">
-              <h5 className="card-title">Code Quiz</h5>
-              <div className="mt-4">
-                <a href="#" className="btn btn-primary mb-2">
+              <h5 className="card-title">JavaScript Quiz</h5>
+              <div className="mt-auto">
+                <a href="https://mnettath.github.io/code-quiz/" className="btn btn-primary mb-2">
                   Deployed Page
                 </a>
-                <a href="#" className="btn btn-secondary">
+                <a href="https://github.com/mnettath/code-quiz" className="btn btn-secondary">
                   GitHub Repo
                 </a>
               </div>
@@ -70,19 +70,19 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="card" style={{ width: "16rem", height: "21rem" }}>
+          <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
-              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top"
+              src="src\assets\images\EmployeeCalendar.png"
+              className="card-img-top img-thumbnail"
               alt="..."
             />
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Employee Calendar</h5>
-              <div className="mt-4">
-                <a href="#" className="btn btn-primary mb-2">
+              <div className="mt-auto">
+                <a href="https://mnettath.github.io/employee-calendar/" className="btn btn-primary mb-2">
                   Deployed Page
                 </a>
-                <a href="#" className="btn btn-secondary">
+                <a href="https://github.com/mnettath/employee-calendar" className="btn btn-secondary">
                   GitHub Repo
                 </a>
               </div>
@@ -90,19 +90,19 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="card" style={{ width: "16rem", height: "21rem" }}>
+          <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
-              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top"
+              src="src\assets\images\WeatherApp.png"
+              className="card-img-top img-thumbnail"
               alt="..."
             />
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Weather Forecast</h5>
-              <div className="mt-4">
-                <a href="#" className="btn btn-primary mb-2">
+              <div className="mt-auto">
+                <a href="https://mnettath.github.io/weather-forecast/" className="btn btn-primary mb-2">
                   Deployed Page
                 </a>
-                <a href="#" className="btn btn-secondary">
+                <a href="https://github.com/mnettath/weather-forecast" className="btn btn-secondary">
                   GitHub Repo
                 </a>
               </div>
@@ -110,19 +110,19 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="card" style={{ width: "16rem", height: "21rem" }}>
+          <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
-              src="https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top"
+              src="src\assets\images\NoteTaker.png"
+              className="card-img-top img-thumbnail"
               alt="..."
             />
             <div className="card-body d-flex flex-column">
               <h5 className="card-title">Note Taker</h5>
-              <div className="mt-4">
-                <a href="#" className="btn btn-primary mb-2">
+              <div className="mt-auto">
+                <a href="https://small-business-note-app-bbd345fcd895.herokuapp.com/notes" className="btn btn-primary mb-2">
                   Deployed Page
                 </a>
-                <a href="#" className="btn btn-secondary">
+                <a href="https://github.com/mnettath/note-taker-app" className="btn btn-secondary">
                   GitHub Repo
                 </a>
               </div>
