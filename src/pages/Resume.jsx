6074217f3c6ff91804aec9be50c81c4e14696bcc
Maya N <nettathm@gmail.com>
2023-10-29 13:19:48 -0600

    resume page now has downloadable link and list of proficiencies

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <div className="mt-3 mx-3 vh-100">
+      <h3 className="mb-3">Resume</h3>
+      <div className="resume-text">
+        <p className="mb-5">
+          Download my resume&nbsp;
+          <a href="public\Bootcamp Resume.pdf" download>
+             here
+          </a>
+        </p>
+        <h4>Front-end Proficiencies</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Responsive Design</li>
+          <li>Bootstrap</li>
+          <li>React</li>
+        </ul>
+        <h4>Back-end Proficiencies</h4>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>JSON</li>
+          <li>MongoDB</li>
+          <li>MySQL</li>
+          <li>GraphQL</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
