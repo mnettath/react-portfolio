ad9dd1ad0d6aa69db51bdd7c4fced42632718270
Maya N <nettathm@gmail.com>
2023-11-07 13:41:42 -0700

    added Snig project to portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,12 +7,39 @@ import JavaScriptQuiz from "../assets/images/CodeQuiz.png";
 import EmployeeCalendar from "../assets/images/EmployeeCalendar.png";
 import WeatherForecast from "../assets/images/WeatherApp.png";
 import NoteTaker from "../assets/images/NoteTaker.png";
+import Snif from "../assets/images/Snif.png"
 
 const Portfolio = () => {
   return (
     <div className="container mt-3 mb-3 mx-3">
       <h3 className="mb-3">Portfolio</h3>
       <div className="row gy-5">
+      <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="card" style={{ width: "16rem", height: "25rem" }}>
+            <img
+              src={Snif}
+              className="card-img-top img-thumbnail"
+              alt="eventful sky app concert page"
+            />
+            <div className="card-body d-flex flex-column">
+              <h5 className="card-title">Snif Dog Dating App</h5>
+              <div className="mt-auto">
+                <a
+                  href="https://github.com/nlad218/dog-dating-app"
+                  className="btn btn-primary mb-2"
+                >
+                  Deployed Page
+                </a>
+                <a
+                  href="https://stormy-mesa-49272-df30e734d644.herokuapp.com/"
+                  className="btn btn-secondary"
+                >
+                  GitHub Repo
+                </a>
+              </div>
+            </div>
+          </div>
+          </div>
         <div className="col-sm-6 col-md-4 col-lg-3">
           <div className="card" style={{ width: "16rem", height: "25rem" }}>
             <img
